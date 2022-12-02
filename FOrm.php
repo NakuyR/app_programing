9a5cd8d5d20e4ae1a7985f934086b2adb867a9c2
Jungdh1669 <95516697+NakuyR@users.noreply.github.com>
2022-12-03 01:07:31 +0900

    Add files via upload

--- FOrm.php
+++ FOrm.php
@@ -4,30 +4,31 @@
     <meta charset="utf-8">
     <meta name="keywords" content="​IT Company, ​The backbone for internet business, Company Services, ​Large payments volume or unique business model?, ​What’s included, What We Do, Custom software solutions">
     <meta name="description" content="">
-    <title>register</title>
+    <title>login</title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
-<link rel="stylesheet" href="register.css" media="screen">
+<link rel="stylesheet" href="FOrm.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
     <meta name="generator" content="Nicepage 4.13.4, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i|Cambay:400,400i,700,700i">
-
     <script>
       function input(){
     document.form.submit();
       }
       </script>
+    
     <script type="application/ld+json">{
 		"@context": "http://schema.org",
 		"@type": "Organization",
 		"name": ""
 }</script>
     <meta name="theme-color" content="#5595e8">
-    <meta property="og:title" content="register">
+    <meta property="og:title" content="FOrm">
     <meta property="og:type" content="website">
   </head>
   <body class="u-body u-xl-mode"> 
     <section class="u-align-center u-clearfix u-white u-section-1" id="carousel_2df3">
+      <img class="u-image u-image-default u-preserve-proportions u-image-1" src="/images/Mycampus.gif" alt="" data-image-width="239" data-image-height="52">
       <div class="u-list u-list-1">
         <div class="u-repeater u-repeater-1"></div>
       </div>
@@ -38,34 +39,26 @@
         <div class="u-repeater u-repeater-3"></div>
       </div>
       <div class="u-form u-form-1">
-        <form action="./php/member_register.php" method="post" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" name="form" style="padding: 10px;" enctype="multipart/form-data">
+        <form action="./php/loginCheck.php" method="post" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form"  name="form" style="padding: 10px;" enctype="multipart/form-data">
           <div class="u-form-group u-form-name">
             <label for="name-3ad6" class="u-label">ID</label>
-            <input type="text" id="name-3ad6" name="ID" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="" placeholder="ID">
+            <input type="text" placeholder="ID" id="name-3ad6" name="ID" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
           </div>
           <div class="u-form-group">
             <label for="email-3ad6" class="u-label">Password</label>
-            <input type="text" placeholder="Password" id="email-3ad6" name="Password" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="required">
+            <input type="password" placeholder="Password" id="email-3ad6" name="Password" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="required">
           </div>
-          <div class="u-form-group u-form-group-3">
-            <label for="text-be49" class="u-label u-label-3">이름</label>
-            <input type="text" placeholder="이름" id="text-be49" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="required">
-          </div>
-          <div class="u-form-group u-form-group-4">
-            <label for="text-c96e" class="u-label">학교</label>
-            <input type="text" placeholder="학교" id="text-c96e" name="Univ" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="required">
-          </div>
-          <div class="u-align-right u-form-group u-form-submit">
-            <button type="button" onclick="input()" class="u-active-palette-2-dark-2 u-border-none u-btn u-btn-submit u-button-style u-hover-palette-2-dark-2 u-palette-2-dark-1 u-btn-1">회원가입</button>
+          <div class="u-align-left-lg u-align-left-md u-align-left-sm u-align-left-xl u-align-right-xs u-form-group u-form-submit">
+            <button type="button" onclick="input()" class="u-active-palette-2-dark-2 u-border-none u-btn u-btn-submit u-button-style u-hover-palette-2-dark-2 u-palette-2-dark-1 u-btn-1">로그인<br>
+            </button>
           </div>
         </form>
       </div>
-      <img class="u-image u-image-default u-preserve-proportions u-image-1" src="/images/Mycampus.gif" alt="" data-image-width="239" data-image-height="52">
+      <a href="../register.html" class="u-active-palette-2-dark-2 u-border-none u-btn u-button-style u-hover-palette-2-dark-2 u-palette-2-dark-1 u-btn-2">회원가입</a>
     </section>
     
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-0fc3"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">Made by JDH_20201016</p>
       </div></footer>
-  
 </body></html>
